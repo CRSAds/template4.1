49856807d3543c06a2a6c1c7defacae393276a60
CRSAds <yuri@crsadvertising.com>
2025-05-07 09:34:39 +0200

    Create content.js

---
+++ content.js
@@ -0,0 +1,16 @@
+window.campaignContent = {
+  step1: {
+    title: "Win een E-bike 🎉",
+    image: "assets/fiets.jpg",
+    description: `Nederland staat met 22 miljoen fietsen wereldwijd bekend als hét ultieme fietsland. Fiets jij graag? Beantwoord een paar vragen en maak kans op een nieuwe Gazelle E-bike!`,
+    buttonText: "Ga Verder"
+  },
+  step2: {
+    title: "Onze actievoorwaarden",
+    contentHtml: `
+      Eerst nog wat meer informatie voor we verder gaan. De uitgebreide voorwaarden kunt u onderaan de pagina vinden. We verloten onder de inschrijvingen verschillende mooie prijzen. De winnaars ontvangen een e-mail op het opgegeven adres. We kunnen u deze actie aanbieden mede dankzij onze sponsors. Wij zullen u dan ook graag vrijblijvend mooie aanbiedingen sturen via e-mail of bellen met een uniek aanbod. Door mee te doen met deze campagne worden uw gegevens gedeeld met onze sponsors. Klik <a href="#" id="viewSponsors">hier</a> om de sponsors te bekijken en eventueel per sponsor uw voorkeur aan te geven. U kunt <a href="#" id="optOut">hier klikken</a> om verder te gaan zonder uw data te delen met de sponsors.
+    `,
+    checkboxText: "Ja. Ik ga akkoord met de actievoorwaarden.",
+    buttonText: "Oke, ga verder"
+  }
+};
